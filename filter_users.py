4b6247b4d23af24cca58d95b74ef475ea0da6938
Dirkstar <info@smartsys.de>
2026-08-17 13:53:25 +0200

    filter_users_by_email hinzugefügt

--- filter_users.py
+++ filter_users.py
@@ -20,6 +20,16 @@ def filter_users_by_age(age):
       print(user)
 
 
+def filter_users_by_email(email):
+  with open("users.json", "r") as file:
+      users = json.load(file)
+
+  filtered_users = [user for user in users if user.get("email", "").lower() == email.lower()]
+
+  for user in filtered_users:
+      print(user)
+
+
 if __name__ == "__main__":
     filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
 
